d4293f0495c2179a531941afd063389a71bdb655
scw <scw3812@naver.com>
2019-12-08 09:38:04 +0900

    Initial commit

---
+++ app/src/main/java/com/nugu/jara/LoadingActivity.java
@@ -0,0 +1,23 @@
+package com.nugu.jara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(3000);
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
