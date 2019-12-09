1caeb01eace840a87798af5556b98f5ff9c95d1a
scw <scw3812@naver.com>
2019-12-09 11:21:35 +0900

    Add Activity

--- app/src/main/java/com/nugu/jara/MainActivity.java
+++ app/src/main/java/com/nugu/jara/MainActivity.java
@@ -2,13 +2,28 @@ package com.nugu.jara;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainButton = (Button)findViewById(R.id.main_button);
+
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SleepSettingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
