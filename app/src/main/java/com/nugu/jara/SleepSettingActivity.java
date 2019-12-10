5526c75655051d17ade194c46d099f77d8fb28d0
scw3812 <dlffpraos@naver.com>
2019-12-10 09:36:48 +0900

    activity_setting.xml change

--- app/src/main/java/com/nugu/jara/SleepSettingActivity.java
+++ app/src/main/java/com/nugu/jara/SleepSettingActivity.java
@@ -3,12 +3,19 @@ package com.nugu.jara;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class SleepSettingActivity extends AppCompatActivity {
+
+    private Spinner hourSpinner;
+    private Spinner minuteSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        hourSpinner = (Spinner)findViewById(R.id.hour_setting_spinner);
+        minuteSpinner = (Spinner)findViewById(R.id.minute_setting_spinner);
     }
 }
