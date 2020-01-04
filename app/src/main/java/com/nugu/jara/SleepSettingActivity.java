f4000235cc4d1496431ec96e0e52a237527e5a15
scw3812 <dlffpraos@naver.com>
2020-01-04 22:39:34 +0900

    am, pm 설정 삭제, 시간 스트링 넣기

--- app/src/main/java/com/nugu/jara/SleepSettingActivity.java
+++ app/src/main/java/com/nugu/jara/SleepSettingActivity.java
@@ -14,58 +14,57 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class SleepSettingActivity extends AppCompatActivity {
 
-    private Spinner meridiemSleepSpinner, hourSleepSpinner, minuteSleepSpinner, meridiemWakeUpSpinner, hourWakeUpSpinner, minuteWakeUpSpinner;
-    private ArrayAdapter meridiemArrayAdapter, hourArrayAdapter, minuteArrayAdapter;
+    private Spinner hourSleepSpinner, minuteSleepSpinner, hourWakeUpSpinner, minuteWakeUpSpinner;
+    private ArrayAdapter hourArrayAdapter, minuteArrayAdapter;
     private Button settingButton;
     private static View v;
-    private static WindowManager mWindowManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        meridiemSleepSpinner = (Spinner)findViewById(R.id.meridiem_setting_sleep_spinner);
         hourSleepSpinner = (Spinner)findViewById(R.id.hour_setting_sleep_spinner);
         minuteSleepSpinner = (Spinner)findViewById(R.id.minute_setting_sleep_spinner);
-        meridiemWakeUpSpinner = (Spinner)findViewById(R.id.meridiem_setting_wakeup_spinner);
         hourWakeUpSpinner = (Spinner)findViewById(R.id.hour_setting_wakeup_spinner);
         minuteWakeUpSpinner = (Spinner)findViewById(R.id.minute_setting_wakeup_spinner);
         settingButton = (Button)findViewById(R.id.setting_button);
 
-        String[] meridiemArray = getResources().getStringArray(R.array.meridiem_array);
         String[] hourArray = getResources().getStringArray(R.array.hour_array);
         List<String> minuteArray = new ArrayList<>();
         for(int i = 0; i< 60; i++){
             minuteArray.add(Integer.toString(i));
         }
 
-        meridiemArrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item, meridiemArray);
         hourArrayAdapter =  new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item, hourArray);
         minuteArrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item, minuteArray);
 
-        meridiemSleepSpinner.setAdapter(meridiemArrayAdapter);
         hourSleepSpinner.setAdapter(hourArrayAdapter);
         minuteSleepSpinner.setAdapter(minuteArrayAdapter);
-        meridiemWakeUpSpinner.setAdapter(meridiemArrayAdapter);
         hourWakeUpSpinner.setAdapter(hourArrayAdapter);
         minuteWakeUpSpinner.setAdapter(minuteArrayAdapter);
 
-        final String meridiemSleep = meridiemArray[meridiemSleepSpinner.getSelectedItemPosition()];
         final String hourSleep = hourArray[hourSleepSpinner.getSelectedItemPosition()];
         final String minuteSleep = minuteArray.get(minuteSleepSpinner.getSelectedItemPosition());
-        final String meridiemWakeUp = meridiemArray[meridiemWakeUpSpinner.getSelectedItemPosition()];
         final String hourWakeUp = hourArray[hourWakeUpSpinner.getSelectedItemPosition()];
         final String minuteWakeUp = minuteArray.get(minuteWakeUpSpinner.getSelectedItemPosition());
 
         settingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Date nowDayTime = new Date();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm");
+                String nowDayTimeStr = simpleDateFormat.format(nowDayTime);
+                String sleepTime = nowDayTimeStr.substring(0,9) + " "+hourSleep+":"+minuteSleep;
+                String wakeUpTime = nowDayTimeStr.substring(0,9) + " "+hourSleep+":"+minuteSleep;
                 ImageView lockMode = new ImageView(SleepSettingActivity.this);
                 lockMode.setImageResource(R.mipmap.ic_launcher);
 
